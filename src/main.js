2552609e37093b9df10ccbb86d8edace87a8f200
Igorbrands <igorcantelmo@gmail.com>
2020-06-12 00:10:00 -0300

    :sparkles: Criação do form e estilização basica

---
+++ src/main.js
@@ -0,0 +1,5 @@
+// Conta para calcular valor da sua hora no projeto
+//
+// By: danielhe4rt
+
+valorHora = (valorProjeto / (diasEfetivos * 4 * horasDiarias) ) + ( ( diasFerias * diasEfetivos * horasDiarias ) )
